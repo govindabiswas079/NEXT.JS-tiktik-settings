ca14ea5d0a39c07ab20752f7184642b67a6c1ebe
Anjali Korekar <govindbiswas079@gmail.com>
2023-01-25 19:10:21 +0530

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,35 @@
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);  
+
+  if (isSSR) return null;
+
+  return (
+    <GoogleOAuthProvider clientId={`958374863533-o9uvot8ecr7472f55213o4nu5nrjqabm.apps.googleusercontent.com`}>
+      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
+        <Navbar />
+        <div className='flex gap-6 md:gap-20 '>
+          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+            <Sidebar />
+          </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default MyApp;
